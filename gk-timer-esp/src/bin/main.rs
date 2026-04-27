0b451b9f4b342da0aa7b33233525eb99ea63bdc1
Chris Targett <chris@xlevus.net>
2026-04-27 08:11:19 +0000

    Basic working display

--- gk-timer-esp/src/bin/main.rs
+++ gk-timer-esp/src/bin/main.rs
@@ -7,13 +7,21 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
-use defmt::info;
+use defmt::{error, info, panic as _panic, trace};
+use display_interface::DisplayError;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+use esp_hal;
 use esp_hal::{
-    clock::CpuClock, efuse::KEY_PURPOSE_0, gpio::{Level, Output, OutputConfig}, i2c::master::{Config, I2c}, timer::timg::TimerGroup
+    clock::CpuClock,
+    gpio::{Level, Output, OutputConfig},
+    i2c::master::{Config, I2c},
+    timer::timg::TimerGroup,
 };
-use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::DisplayRotation, prelude::DisplaySize128x32};
+use ssd1306::{
+    I2CDisplayInterface, Ssd1306Async, mode::DisplayConfigAsync, mode::TerminalModeError,
+    prelude::DisplayRotation, prelude::DisplaySize128x32,
+};
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -25,7 +33,6 @@ extern crate alloc;
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
-
 
 #[embassy_executor::task]
 async fn blink(mut pin: Output<'static>) {
@@ -42,7 +49,6 @@ async fn blink(mut pin: Output<'static>) {
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
     // generator version: 1.1.0
-
     rtt_target::rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
@@ -51,10 +57,8 @@ async fn main(spawner: Spawner) -> ! {
     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
 
     let timg0 = TimerGroup::new(p.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(p.SW_INTERRUPT);
+    let sw_interrupt = esp_hal::interrupt::software::SoftwareInterruptControl::new(p.SW_INTERRUPT);
     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
-
 
     info!("Embassy initialized!");
 
@@ -67,21 +71,44 @@ async fn main(spawner: Spawner) -> ! {
     let _ = spawner;
     let led = Output::new(p.GPIO21, Level::High, OutputConfig::default());
 
-    // let i2c = I2c::new(p.I2C0, Config::default()).unwrap()
-    //     .with_scl(p.GPIO8)
-    //     .with_sda(p.GPIO9)
-    //     .into_async();
+    let i2c = match I2c::new(p.I2C0, Config::default()) {
+        Ok(res) => res.with_scl(p.GPIO9).with_sda(p.GPIO8).into_async(),
+        Err(_) => {
+            _panic!("Unable to set up i2c");
+        }
+    };
 
-    // let interface = I2CDisplayInterface::new(i2c);
-    // let mut display= Ssd1306Async::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
-    //     .into_terminal_mode();
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306Async::new(interface, DisplaySize128x32, DisplayRotation::Rotate0)
+        .into_terminal_mode();
 
-    // display.init().await.unwrap();
+    match display.init().await {
+        Ok(_) => {
+            info!("Display initialised");
+            let _ = display.clear().await;
+        },
+        Err(v) => match v {
+            TerminalModeError::InterfaceError(DisplayError::InvalidFormatError) => {
+                error!("Interface Error(InvalidFormat)")
+            }
+            TerminalModeError::InterfaceError(DisplayError::BusWriteError) => {
+                error!("Interface Error(BusWriteError)")
+            }
+            TerminalModeError::InterfaceError(_) => {
+                error!("Interface Error(Other)")
+            }
+            TerminalModeError::OutOfBounds => error!("OutOfBounds"),
+            TerminalModeError::Uninitialized => error!("Unitialized"),
+        },
+    }
 
     spawner.spawn(blink(led.into())).ok();
 
     loop {
         info!("Hello world!");
+        let _ = display.write_str("Tik").await;
+        Timer::after(Duration::from_secs(1)).await;
+        let _ = display.write_str("Tok").await;
         Timer::after(Duration::from_secs(1)).await;
     }
 
